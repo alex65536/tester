e828a7369204dabfeb8a8d0d948dcf1451d9d037
alex65536 <sh200105@mail.ru>
2017-01-24 19:40:49 +0300

    Fixed typos in build scripts

--- build/build-win32.bat
+++ build/build-win32.bat
@@ -2,7 +2,7 @@
 rem Builds tester for win32.
 cd ..\timerlib
 call build-win32.bat
-call build-win32dll.bat
+call build-win32-dll.bat
 cd ..\src
 lazbuild tester.lpi --os=win32 --cpu=i386
 cd ..\build
--- build/build-win64.bat
+++ build/build-win64.bat
@@ -2,7 +2,7 @@
 rem Builds tester for win64.
 cd ..\timerlib
 call build-win64.bat
-call build-win64dll.bat
+call build-win64-dll.bat
 cd ..\src
 lazbuild tester.lpi --os=win64 --cpu=x86_64
 cd ..\build

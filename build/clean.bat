e635288c082662753e3fa99d58ad968a6cccf15a
Alexander Kernozhitsky <sh200105@mail.ru>
2017-09-01 17:23:51 +0300

    Changed cleaning system

--- build/clean.bat
+++ build/clean.bat
@@ -1,9 +1,18 @@
 @echo off
 rem Cleans the directories.
 cd ..
+
+del /S /Q src\*.exe
+
+del /S /Q timerlib\*.a
+del /S /Q timerlib\*.dll
+
 del /S /Q src\lib
 del /S /Q src\backup
-del /S /Q src\*.exe
-del /S /Q timerlib\*.a
-del /S /Q *.dll
+
+for /D %%i in (src\*) do (
+	del /S /Q "%%i\lib" 
+	del /S /Q "%%i\backup"
+)
+
 cd build
--- build/clean.sh
+++ build/clean.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 # Cleans the directories.
 cd ..
-rm -rf src/lib/
-rm -rf src/backup/
+
 rm -f src/tester-x86_64-linux
 rm -f src/tester-i386-linux
+
 rm -f timerlib/*.a
 rm -f timerlib/*.so
+
+rm -rf src/lib/
+rm -rf src/backup/
+
+for i in src/* ; do
+	rm -rf "$i/lib"
+	rm -rf "$i/backup"
+done
+
 cd build
 

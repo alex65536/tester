24b013485446f1e1e72aac7965c1be1073947f2b
Alexander Kernozhitsky <sh200105@mail.ru>
2018-01-03 19:40:31 +0300

    Fixed years in some recently changed files.

--- src/testerframes.pas
+++ src/testerframes.pas
@@ -1,7 +1,7 @@
 {
   This file is part of Tester
 
-  Copyright (C) 2017 Alexander Kernozhitsky <sh200105@mail.ru>
+  Copyright (C) 2017-2018 Alexander Kernozhitsky <sh200105@mail.ru>
 
   This program is free software; you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the Free
